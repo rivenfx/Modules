9cadadb0e580d5038ae75041bca0a3e3cca76c05
staneee <yi.hang@live.com>
2021-06-30 14:00:26 +0800

    修改注释和表限制

--- src/Riven.ListViewInfo.EntityFrameworkCore/RivenListViewInfoDbContextExtensions.cs
+++ src/Riven.ListViewInfo.EntityFrameworkCore/RivenListViewInfoDbContextExtensions.cs
@@ -10,15 +10,36 @@ namespace Riven
 {
     public static class RivenListViewInfoDbContextExtensions
     {
-        public static ModelBuilder ConfigRivenListViewInfo(this ModelBuilder modelBuilder)
+        /// <summary>
+        /// 配置ListView模块实体映射表信息
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        /// <returns></returns>
+        public static ModelBuilder ConfiurationRivenListViewInfo(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<PageFilterItem>((entityBuilder) =>
             {
                 entityBuilder.HasIndex(o => new { o.Name, o.Creator, o.TenantName });
+                entityBuilder.Property(o => o.Name).IsRequired().HasMaxLength(128);
+                entityBuilder.Property(o => o.Field).IsRequired().HasMaxLength(128);
+
+                entityBuilder.Property(o => o.Creator).IsRequired().HasMaxLength(256);
+                entityBuilder.Property(o => o.TenantName).IsRequired().HasMaxLength(256);
+
+                entityBuilder.Property(o => o.Hidden).HasConversion<byte>();
+               
             });
             modelBuilder.Entity<PageColumnItem>((entityBuilder) =>
             {
                 entityBuilder.HasIndex(o => new { o.Name, o.Creator, o.TenantName });
+
+                entityBuilder.Property(o => o.Name).IsRequired().HasMaxLength(128);
+                entityBuilder.Property(o => o.Field).IsRequired().HasMaxLength(128);
+
+                entityBuilder.Property(o => o.Creator).IsRequired().HasMaxLength(256);
+                entityBuilder.Property(o => o.TenantName).IsRequired().HasMaxLength(256);
+
+                entityBuilder.Property(o => o.Hidden).HasConversion<byte>();
             });
 
 
