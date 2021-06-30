f2d947745962826b15e907473a2a5ba72d79be4b
staneee <yi.hang@live.com>
2021-06-30 10:03:03 +0800

    增加新类库

---
+++ src/Riven.ListViewInfo.EntityFrameworkCore/RivenListViewInfoEntityFrameworkCoreModule.cs
@@ -0,0 +1,13 @@
+using Riven.Modular;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riven
+{
+    public class RivenListViewInfoEntityFrameworkCoreModule : AppModule
+    {
+
+    }
+}
